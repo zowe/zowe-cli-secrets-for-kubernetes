da5bfd3a74a2fb100b4259674ee7ef745e93ce88
Rudy Flores <68666202+rudyflores@users.noreply.github.com>
2023-04-04 16:49:25 -0400

    fix import on vscode package
    Signed-off-by: Rudy Flores <68666202+rudyflores@users.noreply.github.com>

--- packages/vscode/src/extension.ts
+++ packages/vscode/src/extension.ts
@@ -1,6 +1,6 @@
 import { ICredentialManagerConstructor } from "@zowe/imperative";
 import * as vscode from "vscode";
-import { K8sCredentialManager } from "@zowe/secrets-for-kubernetes-for-zowe-cli/src/credentials/K8sCredentialManager";
+import K8sCredentialManager = require("@zowe/secrets-for-kubernetes-for-zowe-cli/src/credentials/K8sCredentialManager");
 
 export function activate(
     context: vscode.ExtensionContext
