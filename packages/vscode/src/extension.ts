bd5c459bea3c92af649ddd8a0b3d509a6a38e817
Rudy Flores <68666202+rudyflores@users.noreply.github.com>
2023-04-10 16:05:46 -0400

    fix bundling issues
    Signed-off-by: Rudy Flores <68666202+rudyflores@users.noreply.github.com>

--- packages/vscode/src/extension.ts
+++ packages/vscode/src/extension.ts
@@ -1,10 +1,7 @@
-import { ICredentialManagerConstructor } from "@zowe/imperative";
 import * as vscode from "vscode";
 import K8sCredentialManager = require("../../cli/src/credentials/K8sCredentialManager");
 
-export function activate(
-    context: vscode.ExtensionContext
-): ICredentialManagerConstructor {
+export function activate(context: vscode.ExtensionContext): any {
     return K8sCredentialManager;
 }
 
