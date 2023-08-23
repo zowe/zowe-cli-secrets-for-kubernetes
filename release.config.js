5b7f97220e9502370951c1f9e05242ff9dd00bbd
Rudy Flores <68666202+rudyflores@users.noreply.github.com>
2023-08-23 14:44:50 -0400

    create pre-release
    Signed-off-by: Rudy Flores <68666202+rudyflores@users.noreply.github.com>

--- release.config.js
+++ release.config.js
@@ -1,24 +1,59 @@
 module.exports = {
     branches: [
         {
-            name: "master",
-            level: "patch",
-            devDependencies: {
-                "@zowe/imperative": "zowe-v1-lts"
-            }
-        }
+            name: "main",
+            channel: "next",
+            level: "minor",
+            prerelease: true,
+        },
     ],
     plugins: [
-        "@octorelease/changelog",
-        ["@octorelease/npm", {
-            aliasTags: {
-                latest: ["zowe-v1-lts"]
+        [
+            "@octorelease/changelog",
+            {
+                displayNames: {
+                    "secrets-for-kubernetes-for-zowe-cli":
+                        "Kubernetes secrets plugin for Zowe CLI",
+                    "secrets-for-kubernetes-for-zowe-explorer":
+                        "Kubernetes secrets plugin for Zowe Explorer",
+                },
+                headerLine: "## TBD Release",
             },
-            pruneShrinkwrap: true
-        }],
-        ["@octorelease/github", {
-            checkPrLabels: true
-        }],
-        "@octorelease/git"
-    ]
+        ],
+        [
+            "@octorelease/lerna",
+            {
+                // Use Lerna only for versioning and publish packages independently
+                npmPublish: false,
+            },
+        ],
+        [
+            "@octorelease/npm",
+            {
+                $cwd: "packages/cli",
+                aliasTags: {
+                    latest: ["zowe-k8s-v1-lts"],
+                },
+                npmPublish: true,
+                tarballDir: "dist",
+            },
+        ],
+        [
+            "@octorelease/vsce",
+            {
+                $cwd: "packages/vscode",
+                ovsxPublish: true,
+                vscePublish: true,
+                vsixDir: "dist",
+            },
+        ],
+        [
+            "@octorelease/github",
+            {
+                assets: ["dist/*.tgz", "dist/*.vsix"],
+                draftRelease: true,
+            },
+        ],
+        "@octorelease/git",
+    ],
 };
