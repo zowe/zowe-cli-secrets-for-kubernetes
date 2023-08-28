2b6b047b9ba5cb44dd348e319113e881bb41c7f2
Rudy Flores <68666202+rudyflores@users.noreply.github.com>
2023-08-28 12:33:16 -0400

    Revert "Kubernetes Plugin - Pre-release v0.0.1"

--- release.config.js
+++ release.config.js
@@ -1,59 +1,24 @@
 module.exports = {
     branches: [
         {
-            name: "main",
-            channel: "next",
-            level: "minor",
-            prerelease: true,
-        },
+            name: "master",
+            level: "patch",
+            devDependencies: {
+                "@zowe/imperative": "zowe-v1-lts"
+            }
+        }
     ],
     plugins: [
-        [
-            "@octorelease/changelog",
-            {
-                displayNames: {
-                    "secrets-for-kubernetes-for-zowe-cli":
-                        "Kubernetes secrets plugin for Zowe CLI",
-                    "secrets-for-kubernetes-for-zowe-explorer":
-                        "Kubernetes secrets plugin for Zowe Explorer",
-                },
-                headerLine: "## TBD Release",
+        "@octorelease/changelog",
+        ["@octorelease/npm", {
+            aliasTags: {
+                latest: ["zowe-v1-lts"]
             },
-        ],
-        [
-            "@octorelease/lerna",
-            {
-                // Use Lerna only for versioning and publish packages independently
-                npmPublish: false,
-            },
-        ],
-        [
-            "@octorelease/npm",
-            {
-                $cwd: "packages/cli",
-                aliasTags: {
-                    latest: ["zowe-v1-lts"],
-                },
-                npmPublish: true,
-                tarballDir: "dist",
-            },
-        ],
-        [
-            "@octorelease/vsce",
-            {
-                $cwd: "packages/vscode",
-                ovsxPublish: true,
-                vscePublish: true,
-                vsixDir: "dist",
-            },
-        ],
-        [
-            "@octorelease/github",
-            {
-                assets: ["dist/*.tgz", "dist/*.vsix"],
-                draftRelease: true,
-            },
-        ],
-        "@octorelease/git",
-    ],
+            pruneShrinkwrap: true
+        }],
+        ["@octorelease/github", {
+            checkPrLabels: true
+        }],
+        "@octorelease/git"
+    ]
 };
