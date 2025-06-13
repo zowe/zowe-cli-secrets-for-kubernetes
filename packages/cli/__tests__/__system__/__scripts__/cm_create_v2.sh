3dd3b58b0cfda6175fc41cdfddd570a17ec426ae
Timothy Johnson <timothy.johnson@broadcom.com>
2025-06-13 11:21:02 -0400

    Store credentials in project_base profile
    Signed-off-by: Timothy Johnson <timothy.johnson@broadcom.com>

--- packages/cli/__tests__/__system__/__scripts__/cm_create_v2.sh
+++ packages/cli/__tests__/__system__/__scripts__/cm_create_v2.sh
@@ -3,8 +3,8 @@ set -e
 
 zowe config init --prompt=false > /dev/null 2>&1
 
-zowe config set profiles.base.properties.user USER
-zowe config set profiles.base.properties.password PLAINTEXT
+zowe config set profiles.project_base.properties.user USER
+zowe config set profiles.project_base.properties.password PLAINTEXT
 
 zowe config list "profiles" --rfj
 
